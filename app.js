f34d260b0a929a02154c7ef98de895c71e4149a1
NaveeraRajChuhan <nnfstars@gmail.com>
2025-04-08 01:20:40 +0500

    final done

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-var names =[ "nazish","naveera","filza","hira","hassan"]
-var copyarr = names.slice(0,3)
-// document.write(copyarr)
-if (copyarr == ["nazish","naveera","filza"]){
-    document.write("sahi chala")
-}else{
-    document.write("galat chala")
+// var names =[ "nazish","naveera","filza","hira","hassan"]
+// var copyarr = names.slice(0,3)
+// // document.write(copyarr)
+// // console.log(copyarr);
+
+// if (copyarr === "nazish","naveera","filza"){
+//     document.write("sahi chala")
+// }else{
+//     document.write("galat chala")
+// }
+
+
+
+// var names = ["nazish", "naveera", "filza", "hira", "hassan"];
+// var copyarr = names.slice(0, 3);
+
+// console.log(copyarr);
+
+// // Check if the sliced array is equal to the expected array ["nazish", "naveera", "filza"]
+// if (copyarr.every((val, index) => val === ["nazish", "naveera", "filza"][index])) {
+//     document.write("sahi chala");
+// } else {
+//     document.write("galat chala");
+// }
+
+
+
+
+
+
+
+var names = ["nazish", "naveera", "filza", "hira", "hassan"];
+var copyarr = names.slice(0, 3); 
+
+// console.log(copyarr);
+
+if (copyarr[0] === "nazish" && copyarr[1] === "naveera" && copyarr[2] === "filza") {
+    document.write("sahi chala");
+} else {
+    document.write("galat chala");
 }
+
+
+
+
