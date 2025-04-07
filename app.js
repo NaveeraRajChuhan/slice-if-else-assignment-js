9d7a5168262c67c5b7208ec4e923347eb6c14cda
NaveeraRajChuhan <nnfstars@gmail.com>
2025-04-07 23:44:31 +0500

    if else

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 var names =[ "nazish","naveera","filza","hira","hassan"]
 var copyarr = names.slice(0,3)
 document.write(copyarr)
+if (copyarr = "nazish","naveera","filza"){
+    document.write("sahi chala")
+}else{
+    document.write("galat chala")
+}
