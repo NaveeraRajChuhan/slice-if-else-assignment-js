5cc426f24094b271877ac183abc89d99cfc07a56
NaveeraRajChuhan <nnfstars@gmail.com>
2025-04-07 23:46:50 +0500

    correction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var names =[ "nazish","naveera","filza","hira","hassan"]
 var copyarr = names.slice(0,3)
 // document.write(copyarr)
-if (copyarr = "nazish","naveera","filza"){
+if (copyarr == ["nazish","naveera","filza"]){
     document.write("sahi chala")
 }else{
     document.write("galat chala")
